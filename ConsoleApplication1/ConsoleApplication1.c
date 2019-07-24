374c9311ee9aba4c81ac885839a42eea95dafc06
imnotasophist <romandub25@gmail.com>
2019-07-24 17:56:45 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <ctype.h>
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int guess = 50;
+	int max = 100, min = 0;
+
+	printf("Загадайте число от 1 до 100\n");
+	printf("Ваше число %d?\n", guess);
+
+	while (getchar() != 'y') {
+		printf("Ваше число больше?\n");
+		getchar();
+		if (getchar() == 'y') {
+			min = guess;
+			guess += (max - min) / 2;
+		}
+		else {
+			max = guess;
+			guess -= (max - min) / 2;
+		}
+		printf("Ваше число %d?\n", guess);
+		while (getchar() != '\n') continue;
+	}
+
+	return 0;
+}
